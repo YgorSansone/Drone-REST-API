915936fe86068ae6837c145260105c19efb2ae54
Ygor Sansone <ygorsansone@gmail.com>
2020-09-04 10:55:56 -0300

    Limpando o código

--- modelsdb/model.js
+++ modelsdb/model.js
@@ -62,39 +62,5 @@ const Drone = sequelize.define("drone", {
 
 sequelize.sync().then(() => {
   console.log(`Database & tables created!`);
-
-  Drone.bulkCreate([
-    {
-      id: 1,
-      custumer_image:
-        "https://i.pinimg.com/originals/95/20/21/95202129c0b8d430b8ea288a73c1f53d.jpg",
-      customer_name: "Ygor",
-      customer_address: "Rua dos bobos, numero 0",
-      battery: 100,
-      max_speed: 80,
-      average_speed: 84.3,
-      status: "success",
-      current_fly: 10,
-    },
-    {
-      id: 2,
-      custumer_image:
-        "https://images.unsplash.com/photo-1462206092226-f46025ffe607?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-      customer_name: "Pedro",
-      customer_address: "Av Brasil",
-      battery: 50,
-      max_speed: 4.8,
-      average_speed: 10.6,
-      status: "repair",
-      current_fly: 94,
-    },
-  ])
-    .then(function () {})
-    .catch((error) =>
-      res.json({
-        error: true,
-        error: error,
-      })
-    );
 });
 module.exports = Drone
